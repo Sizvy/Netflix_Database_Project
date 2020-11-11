dc3b686add8ee20bb45bfec111fd37b30eb96234
Swapnil Dey <60988345+swapped004@users.noreply.github.com>
2020-11-12 02:03:54 +0600

    A better search with show title, genre, actors and directors

--- home/views.py
+++ home/views.py
@@ -60,39 +60,67 @@ def home_LoggedIn(request,user_ID):
 def search(request):
 
     search_item = request.GET.get('search')
+
+    search_pattern = "%"+search_item.lower()+"%"
+
     print(search_item)
 
     cursor = connection.cursor()
-    sql = "SELECT * FROM SHOW s, DIRECTOR d WHERE s.DIRECTOR_ID = d.PERSON_ID"
-    cursor.execute(sql)
+    sql = "SELECT DISTINCT(s.SHOW_ID) FROM SHOW s, DIRECTOR d,ACTOR a,ACT ac" \
+          " WHERE s.DIRECTOR_ID = d.PERSON_ID" \
+          " AND s.SHOW_ID = ac.SHOW_IDACT AND ac.ACTOR_IDACT = a.PERSON_ID AND " \
+          "(" \
+          "LOWER(s.TITLE) like %s" \
+          " OR LOWER(d.DIRECTOR_FIRST_NAME || '' || d.DIRECTOR_LAST_NAME) like %s" \
+          " OR LOWER(a.ACTOR_FIRST_NAME || '' || a.ACTOR_LAST_NAME) like %s" \
+          " OR LOWER(s.GENRE) like %s)"
+
+    print(sql)
+
+    cursor.execute(sql, [search_pattern, search_pattern, search_pattern, search_pattern])
     result = cursor.fetchall()
     cursor.close()
 
     error_msg = "No Result Found!"
     show_list=[]
     for r in result:
-        temp_director = str(r[15])+" "+str(r[16])
-        temp_show = r[2]
-        if search_item.lower() == temp_director.lower() or search_item.lower() == temp_show.lower():
-            print("milse")
-            show_title = r[2]
-            show_genre = r[1]
-            show_des = r[3]
-            show_age = r[4]
-            show_lang = r[8]
-            show_image = r[13]
-            show_imdb = r[5]
-            director_name = temp_director
-            director_link = r[18]
-            single_row = {"director_name":director_name,"director_link":director_link,"show_imdb":show_imdb,"show_title":show_title,"show_genre":show_genre,"show_des":show_des,"show_age":show_age,"show_lang":show_lang,"show_image":show_image}
+
+        show_id = r[0]
+        print(show_id)
+        cursor = connection.cursor()
+        sql = 'SELECT * FROM SHOW WHERE SHOW_ID = %s'
+        cursor.execute(sql, [show_id])
+        show_res = cursor.fetchall()
+        cursor.close()
+
+        for r_temp in show_res:
+            show_title = r_temp[2]
+            show_genre = r_temp[1]
+            show_des = r_temp[3]
+            show_age = r_temp[4]
+            show_lang = r_temp[8]
+            show_image = r_temp[13]
+            show_imdb = r_temp[5]
+            single_row = {"show_imdb": show_imdb,
+                          "show_title": show_title,
+                          "show_genre": show_genre,
+                          "show_des": show_des,
+                          "show_age": show_age,
+                          "show_lang": show_lang,
+                          "show_image": show_image}
             show_list.append(single_row)
             error_msg = ""
+
+
     show_all = {
         "show_name":search_item,
         "shows":show_list,
         "error_msg":error_msg
     }
-    return render(request,'home\homepage.html',{'shows' : show_list , 'error_msg' : error_msg})
+    return render(request, 'home\homepage.html', {'shows': show_list , 'error_msg': error_msg})
+
+
+
 
 def log_out(request):
     if request.session.get('is_logged_in',False) == True:
