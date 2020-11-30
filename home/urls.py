bcbb77c9ab431f471afb150ed68bb6eecf4e1396
Swapnil Dey <60988345+swapped004@users.noreply.github.com>
2020-11-30 22:43:53 +0600

    added profile view, added time in rating, added subscription list and added favourite genre using pl/sql procedure

--- home/urls.py
+++ home/urls.py
@@ -28,7 +28,9 @@ urlpatterns = [
     path('movies/<str:show_id>/', views.single_show, name="single_show"),
     path('series/<str:series_identifier>/', views.single_series, name="single_series"),
     path('subscribe/<str:show_identifier>/', views.subscribe_show, name="subscribe_show"),
+    path('home/subscribed_show/',views.subscribed_show,name="subscribed_show"),
     path('home/settings/',views.settings,name="settings"),
+    path('profile/<str:user_id>/',views.profile_show,name="profile_show")
 
 
 
